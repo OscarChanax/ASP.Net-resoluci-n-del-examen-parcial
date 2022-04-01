44664361e11d495e37ff2053839f5a94edcbc514
oscar chanax <oscar2001chanax@hotmail.com>
2022-04-01 17:22:34 -0600

    Sección 1

---
+++ Alumno.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP.Net_resolución_del_examen_parcial
+{
+    public class Alumno
+    {
+        public string carne { get; set; }
+        public string nombre { get; set; }
+    }
+}
---
+++ Inscripciones.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP.Net_resolución_del_examen_parcial
+{
+    public class Inscripciones
+    {
+        public string carne { get; set;}
+        public int grado { get; set;}
+        public DateTime fecha { get; set; }
+    }
+}
