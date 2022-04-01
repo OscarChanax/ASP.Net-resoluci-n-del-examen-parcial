8d0d3ed5f23d1d6ffb8a3a815fa5b693ae444281
oscar chanax <oscar2001chanax@hotmail.com>
2022-04-01 17:43:21 -0600

    Sección 2

---
+++ Reporte.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP.Net_resolución_del_examen_parcial
+{
+    public class Reporte
+    {
+        public string Nombre { get; set; }
+        public int Grado { get; set; }
+
+    }
+}
